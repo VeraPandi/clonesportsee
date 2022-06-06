8869faf058f3a30b550e4426defc33e845b1e7f4
VeraPandi <84042427+VeraPandi@users.noreply.github.com>
2022-06-06 07:19:05 +0200

    Add: 404 page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+   BrowserRouter as Router,
+   Routes,
+   Route,
+   Navigate,
+} from "react-router-dom";
 import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
@@ -6,11 +11,12 @@ import "./styles/main.scss";
 
 const App = () => {
    return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
          <Header />
          <Routes>
             <Route path="/user/:id" element={<Profile />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<Navigate replace to="/404" />} />
          </Routes>
       </Router>
    );
